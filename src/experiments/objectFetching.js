481cfc13236d1695553acc3fbe593d8cb42f806b
Pavel Hamrik <pavel.hamrik@gmail.com>
2018-11-25 19:23:22 +0100

    fetching map vs. array of object

--- src/experiments/objectFetching.js
+++ src/experiments/objectFetching.js
@@ -1,15 +1,15 @@
 export const fetchObjects = () => {
-    const content = Array.from(Array(10000), (val, idx) => (
-        [idx, 'foo']
+    const content = Array.from(Array(100000), (val, idx) => (
+        [idx, {foo: 'bar', ambitious: 'leprechaun'}]
     ));
 
     const map = new Map(content);
     console.log('map', map);
 
-    const array = content.map(item => ({id: item[0], val: item[1]}));
+    const array = content.map(item => (Object.assign({}, item[1], {id: item[0]})));
     console.log('array', array);
 
-    const id = Math.random() * (content.length - 1);
+    const id = Math.round(Math.random() * (content.length - 1));
 
     console.time('fetch from map');
     console.log(map.get(id));
